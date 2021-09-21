5f766901692ee957d97e26ff6f8e97bb68f48e36
Tero Kaihula <N0465@student.jamk.fi>
2021-09-21 08:09:54 +0300

    gatsby-travel-website

---
+++ src/pages/images.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Image from "../components/image"
+
+const Images = () => (
+  <Layout>
+    <Seo title="Images" />
+    <Image />
+  </Layout>
+)
+
+export default Images
